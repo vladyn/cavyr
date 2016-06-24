9964ab315e1f7e42289f73a2bc87457ee43c5f9d
Vladimir Varbanov <vladimir.varbanov@paddypower.com>
2016-06-24 15:13:31 +0300

    adding basic product list

--- app/scripts/controllers/product.js
+++ app/scripts/controllers/product.js
@@ -3,5 +3,30 @@
 angular.module('cavyrApp')
     .controller('ProductCtrl', function() {
         this.selectedProduct = null;
+        this.products = {
+          p1: {
+            name: "product 1",
+            type: "type"
+          },
+          p2: {
+            name: "product 2",
+            type: "type"
+          },
+          p3: {
+            name: "product 3",
+            type: "type"
+          },
+          p4: {
+            name: "product 5",
+            type: "type"
+          },
+          p5: {
+            name: "product 5",
+            type: "type"
+          },
+          p6: {
+            name: "product 6",
+            type: "type"
+          }
+        };
     });
-    
