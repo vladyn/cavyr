d5730658a8fd1564116228355b15104862fe8fda
Vladimir Varbanov <vladimir.varbanov@paddypower.com>
2016-06-28 18:02:53 +0300

    adding more images

--- app/scripts/controllers/product.js
+++ app/scripts/controllers/product.js
@@ -5,36 +5,41 @@ angular.module('cavyrApp')
         this.selectedProduct = null;
         this.products = {
           p1: {
-            name: "product 1",
+            name: "Toshiba",
             type: "type",
             image: "images/toshiba-satellite-c875-14w_small.png"
           },
           p2: {
-            name: "product 2",
+            name: "Asus",
             type: "type",
             image: "images/asus_small.jpeg"
           },
           p3: {
-            name: "product 3",
+            name: "HP ColorBook",
             type: "type",
-            image: "images/5328cd5e3a4bdee9.jpg"
+            image: "images/hp_001.jpg"
 
           },
           p4: {
-            name: "product 5",
+            name: "HP",
             type: "type",
-            image: "images/5328cd5e3a4bdee9.jpg"
+            image: "images/hp_333.jpg"
 
           },
           p5: {
-            name: "product 5",
+            name: "Lenovo",
             type: "type",
-            image: "images/5328cd5e3a4bdee9.jpg"
+            image: "images/lenovo.jpg"
           },
           p6: {
             name: "product 6",
             type: "type",
-            image: "images/5328cd5e3a4bdee9.jpg"
+            image: "images/lg.jpg"
+          },
+          p7: {
+            name: "Philips",
+            type: "type",
+            image: "images/philips.jpg"
           }
         };
     });
