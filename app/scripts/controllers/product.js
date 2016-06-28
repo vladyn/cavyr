a57178a17b97790d3f122f1dbe95e7655a8ab137
Vladimir Varbanov <vladimir.varbanov@paddypower.com>
2016-06-28 17:45:38 +0300

    adding eslint and some iamges

--- app/scripts/controllers/product.js
+++ app/scripts/controllers/product.js
@@ -6,27 +6,35 @@ angular.module('cavyrApp')
         this.products = {
           p1: {
             name: "product 1",
-            type: "type"
+            type: "type",
+            image: "images/toshiba-satellite-c875-14w_small.png"
           },
           p2: {
             name: "product 2",
-            type: "type"
+            type: "type",
+            image: "images/asus_small.jpeg"
           },
           p3: {
             name: "product 3",
-            type: "type"
+            type: "type",
+            image: "images/5328cd5e3a4bdee9.jpg"
+
           },
           p4: {
             name: "product 5",
-            type: "type"
+            type: "type",
+            image: "images/5328cd5e3a4bdee9.jpg"
+
           },
           p5: {
             name: "product 5",
-            type: "type"
+            type: "type",
+            image: "images/5328cd5e3a4bdee9.jpg"
           },
           p6: {
             name: "product 6",
-            type: "type"
+            type: "type",
+            image: "images/5328cd5e3a4bdee9.jpg"
           }
         };
     });
